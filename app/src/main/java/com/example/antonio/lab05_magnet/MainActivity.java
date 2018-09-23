61874e8d0ec946e20172646af6301ccc6a1f1ec7
freon-lunarion <antonio.daniswara@gmail.com>
2018-09-23 12:00:55 +1000

    task 1 - the x, y and z axis and display the values on the screen continuously

--- app/src/main/java/com/example/antonio/lab05_magnet/MainActivity.java
+++ app/src/main/java/com/example/antonio/lab05_magnet/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.antonio.lab05_magnet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnTask_1,btnTask_2, btnTask_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTask_1 = findViewById(R.id.btnTask_1);
+        btnTask_2 = findViewById(R.id.btnTask_2);
+        btnTask_3 = findViewById(R.id.btnTask_3);
+
+        btnTask_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchTask1(view);
+            }
+        });
     }
+
+    private void switchTask1(View view) {
+        Intent intent = new Intent(this, Task1Activity.class);
+        startActivity(intent);
+
+    }
+
+
 }
